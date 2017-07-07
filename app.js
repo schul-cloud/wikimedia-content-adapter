9c21f3427051fce4392defd00431861efb833174
Fabien Charlé <fabien.charle@student.hpi.de>
2017-07-07 12:31:38 +0200

    Add UTF-8 encoding for response header

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');		// the restframework
 var app = express();							// create the webapplication
 
-// default value for Address an Port
+// default value for Address and Port
 var address = "localhost";
 var port = 3000;
 
@@ -21,7 +21,8 @@ if(process.argv.length > 2){
 
 // routing for api-v1 : version 1
 app.get("/v1/",function(req,res){
-   var fullUrl = req.protocol + '://' + address +":" + port + req.path;
+	res.set({ 'content-type': 'application/json; charset=utf-8' });
+   	var fullUrl = req.protocol + '://' + address +":" + port + req.path;
 	var wikimedia = require("./wikimedia/Request.js").getRequest(req.query,res,fullUrl,1);	// create a new wikimedia-Request
 	wikimedia.execute();
 });
