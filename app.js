ef98f824eba6f6361ff50f7347b6cc7c8e416483
Fabien Charlé <fabien.charle@student.hpi.de>
2017-09-13 14:01:30 +0200

    Fix accept

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');		// the restframework
 var app = express();							// create the webapplication
-
+const contentType = 'application/vnd.api+json';
 // default value for Address and Port
 var address = "127.0.0.1";
 var port = 3000;
@@ -22,12 +22,11 @@ if(process.argv.length > 2){
 
 // routing for api-v1 : version 1
 app.get("/v1",function(req,res){
-	res = res.set('content-type', 'application/vnd.api+json');
-    var accept = !(typeof req.header("accept") == 'undefined');
-    accept = accept && !(req.header("accept") == " ");
-    accept = accept && !(req.header("accept") == "");
-    accept = accept && req.accepts('application/vnd.api+json') === 'application/vnd.api+json';
-    console.log( accept );
+	res = res.set('content-type', contentType);
+    var accept = !(typeof req.header("accept") === 'undefined');
+    accept = accept && !(req.header("accept") === " ");
+    accept = accept && !(req.header("accept") === "");
+    accept = accept && req.accepts(contentType) === contentType;
    	var fullUrl = req.protocol + '://' + address +":" + port + req.path;
 	require("./wikimedia/Request.js").makeRequest(req.query,fullUrl,accept,
         function (error , status) {
